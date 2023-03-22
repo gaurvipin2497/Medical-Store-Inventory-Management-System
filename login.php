58b3aec5fce89038a87013ff345d0a97134fdc17
gaurvipin2497 <gaurvipin2497@gmail.com>
2023-03-22 12:44:53 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,37 +1,44 @@
 <?php
-	session_start();
-	if( isset($_POST['submit']) && isset($_POST['username']) && isset($_POST['password']) )
-	{
-		include 'config.php';
-		
-		$username = stripslashes($_POST['username']);
-		$password = stripslashes($_POST['password']);
-		
-		if(!($dbconn = @mysql_connect($dbhost, $dbuser, $dbpass))) exit('Error connecting to database.');
-		mysql_select_db($db);
+session_start();
 
-		$getCreds = mysql_query("SELECT role FROM ".$dbtable." WHERE username='".$username."' AND password='".$password."'");
-		$gotCreds = mysql_fetch_array($getCreds);
+if (isset($_POST['submit']) && isset($_POST['username']) && isset($_POST['password'])) {
+  include 'config.php';
 
-		if(strcmp($gotCreds['role'],'med_admin')==0){
-			$_SESSION['med_admin']='adminadmin';
-			header("Location: med_admin_screen.php");
-		}
-		else if($gotCreds['role']=='receptionist'){
-			$_SESSION['receptionist']='receptionistic';
-			header("Location: med_store_reception.php");
-		}
-		else if($gotCreds['role']=='doctor'){
-			$_SESSION['doctor']='doctordoctor';
-			header("Location: med_store_doctor.php");
-		}
-		else
-		{
-			header("Location: index.html");
-		}
-	}
-	else
-	{
-		header("Location: index.html");
-	}
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+
+  try {
+    $pdo = new PDO("mysql:host=$dbhost;dbname=$db", $dbuser, $dbpass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $stmt = $pdo->prepare("SELECT role FROM $dbtable WHERE username = :username AND password = :password");
+    $stmt->bindParam(':username', $username);
+    $stmt->bindParam(':password', $password);
+    $stmt->execute();
+
+    $gotCreds = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    if ($gotCreds['role'] == 'med_admin') {
+      $_SESSION['med_admin'] = 'adminadmin';
+      header("Location: med_admin_screen.php");
+      exit;
+    } else if ($gotCreds['role'] == 'receptionist') {
+      $_SESSION['receptionist'] = 'receptionistic';
+      header("Location: med_store_reception.php");
+      exit;
+    } else if ($gotCreds['role'] == 'doctor') {
+      $_SESSION['doctor'] = 'doctordoctor';
+      header("Location: med_store_doctor.php");
+      exit;
+    } else {
+      header("Location: index.html");
+      exit;
+    }
+  } catch(PDOException $e) {
+    echo "Error connecting to database: " . $e->getMessage();
+  }
+} else {
+  header("Location: index.html");
+  exit;
+}
 ?>
