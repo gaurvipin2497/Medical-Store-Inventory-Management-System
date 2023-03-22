2031328c83849eef335f5887ab36c4c17830e9c6
gaurvipin2497 <gaurvipin2497@gmail.com>
2023-03-22 12:45:55 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,11 +1,14 @@
 <?php
 	$dbhost = 'bloodbank.cq5ahkkmqrau.ap-south-1.rds.amazonaws.com';
-	$db = 'db';
+	$dbname = 'db';
 	$dbtable = 'users';
 	$dbuser = 'admin';
 	$dbpass = 'admin1234';
 
-$db_name = 'db';
-
-$conn = mysqli_connect("bloodbank.cq5ahkkmqrau.ap-south-1.rds.amazonaws.com",$dbuser,$dbpass,$db_name);
+	try {
+		$pdo = new PDO("mysql:host=$dbhost;dbname=$dbname", $dbuser, $dbpass);
+		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	} catch(PDOException $e) {
+		echo "Error connecting to database: " . $e->getMessage();
+	}
 ?>
