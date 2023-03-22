03a00ba28e347f2c719134b13d9264dcb8cf1e5e
gaurvipin2497 <gaurvipin2497@gmail.com>
2023-03-22 12:21:38 +0530

    Update config.php

--- config.php
+++ config.php
@@ -4,4 +4,8 @@
 	$dbtable = 'users';
 	$dbuser = 'admin';
 	$dbpass = 'admin1234';
+
+$db_name = 'db';
+
+$conn = mysqli_connect("bloodbank.cq5ahkkmqrau.ap-south-1.rds.amazonaws.com",$dbuser,$dbpass,$db_name);
 ?>
